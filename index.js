80a85681060e6c15d403e755884491ff15bb88fe
Tammy Metz <tammy@tammymetz.com>
2019-10-01 15:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 // Write your code here!
 
-let element = document.createElement('div');
-document.body.appendChild(element);
-
-let ul = document.createElement('ul')
-
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-element.appendChild(ul)
+let mainEl = document.getElementById("main#main");
+mainEl.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "YOUR-NAME is the champion";
