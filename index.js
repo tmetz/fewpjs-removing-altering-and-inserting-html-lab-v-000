421209b8a56cbdae7b513a5aa9a95b09f29cb08f
Tammy Metz <tammy@tammymetz.com>
2019-10-01 15:53:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let mainEl = document.getElementById("main#main");
 mainEl.remove();
-let newHeader = document.createElement('h1');
+const newHeader = document.createElement('h1');
 newHeader.setAttribute("id", "victory");
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "YOUR-NAME is the champion";
