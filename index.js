2a08dad52864aa5e317c4b6f5b30889b926b1cdd
Tammy Metz <tammy@tammymetz.com>
2019-10-01 15:54:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+let mainEl = document.getElementById("main");
+mainEl.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "YOUR-NAME is the champion";
