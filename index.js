6889d35f22e244aff4fb4581d9a75c0f14dd1728
Tammy Metz <tammy@tammymetz.com>
2019-10-01 15:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let element = document.createElement('div');
 document.body.appendChild(element);
 
 let ul = document.createElement('ul')
- 
+
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li')
   li.innerHTML = (i + 1).toString()
   ul.appendChild(li)
 }
- 
+
 element.appendChild(ul)
